183deb0a58d84eb657cdf74823df73e3a9abdc0e
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-02-08 23:31:08 -0500

    Updates

---
+++ precache-manifest.0d6979b772781fb1418de2c49dae6420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff4ca1fcf57a01dd069fccb8efbb4e8",
+    "url": "/moster-rolodex/index.html"
+  },
+  {
+    "revision": "ecec09547c07ba1ae656",
+    "url": "/moster-rolodex/static/css/main.9008ae4f.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/moster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/moster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecec09547c07ba1ae656",
+    "url": "/moster-rolodex/static/js/main.9f49e40c.chunk.js"
+  },
+  {
+    "revision": "330a9fe46f31ee610d85",
+    "url": "/moster-rolodex/static/js/runtime-main.64e858fc.js"
+  }
+]);
